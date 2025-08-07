56a991dd54f8454a7c35eb534e83bd36b3312bb8
Marcosvbs11 <mvbs9@discente.ifpe.edu.br>
2025-08-07 16:03:43 -0300

    atividade_04

---
+++ Marcos/atividade_04/database/seeders/AuthorPublisherBookSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AuthorPublisherBookSeeder extends Seeder
+{
+    public function run()
+    {
+        // código para popular as tabelas authors, publishers e books
+    }
+}
