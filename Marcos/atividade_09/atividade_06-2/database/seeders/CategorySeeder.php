0ef9cd84760db866aa7e9e5170f6f5d75017e337
Marcosvbs11 <mvbs9@discente.ifpe.edu.br>
2025-08-13 22:58:24 -0300

    atividade_09

---
+++ Marcos/atividade_09/atividade_06-2/database/seeders/CategorySeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run()
+    {
+        // código para popular a tabela categories
+    }
+}
+
